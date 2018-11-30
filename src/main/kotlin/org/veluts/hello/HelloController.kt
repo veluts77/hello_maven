780fee2ef50c3cc33c3cbb71905c81d05336c3b9
vlutsenk <vladimir.lutsenko@t-systems.ru>
2018-11-30 23:00:14 +0300

    Added basic demo functionality.

---
+++ src/main/kotlin/org/veluts/hello/HelloController.kt
@@ -0,0 +1,17 @@
+package org.veluts.hello
+
+import java.time.LocalDateTime
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    @GetMapping("/")
+    fun root() = "This is 'Hello Maven' application!!! Accessed at ${LocalDateTime.now()}."
+	
+    @GetMapping("/about")
+    fun about() = "Created by me!"
+}
