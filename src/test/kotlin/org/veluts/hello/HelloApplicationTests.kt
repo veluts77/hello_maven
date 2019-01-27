79506982364a1c4029f122a76fa7026291cb8aea
veluts <veluts77@gmail.com>
2019-01-27 22:51:22 +0300

    Added few tests (one is failing)

--- src/test/kotlin/org/veluts/hello/HelloApplicationTests.kt
+++ src/test/kotlin/org/veluts/hello/HelloApplicationTests.kt
@@ -1,16 +1,46 @@
 package org.veluts.hello
 
+import org.assertj.core.api.Assertions.*
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.*
 import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.boot.test.web.client.TestRestTemplate
+
 
 @RunWith(SpringRunner::class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class HelloApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Autowired
+    lateinit var controller: HelloController
 
+    @LocalServerPort
+    var port: Int = 0
+
+    @Autowired
+    lateinit var restTemplate: TestRestTemplate
+
+    @Test
+    fun contextLoads() {
+        assertThat(controller).isNotNull
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun defaultShouldReturnDefaultMessage() {
+        assertThat(endpoint("")).startsWith("This is 'Hello Maven' application!!!")
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun aboutShouldReturnDefaultMessage() {
+        assertThat(endpoint("about")).doesNotStartWith("Created by")
+    }
+
+    private fun endpoint(name: String) = restTemplate.getForObject("http://localhost:$port/$name",
+            String::class.java)
 }
